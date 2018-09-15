ebac4fe0d85a34c30f655270fd4e933b72818d02
Robinson Severo <robinsonvs@gmail.com>
2018-09-15 20:40:30 -0300

    commit 15092018 2040

---
+++ Sets/set.js
@@ -0,0 +1,46 @@
+
+/* Sets */
+
+function mySet() {
+    // the var collection will hold the set
+    var collection = [];
+    // this method will check for the presence of an element and return true or false
+    this.has = function(element) {
+        return (collection.indexOf(element) !== -1);
+    }
+    // this method will return all the values in the set
+    this.values = function() {
+        return collection;
+    }
+    // this method will add an element to the set
+    this.add = function(element) {
+        if (!this.has(element)) {
+            collection.push(element);
+            return true;
+        }
+        return false;
+    }
+    // this method will remove an element from a set
+    this.remove = function(element) {
+        if (this.has(element)) {
+            index = collection.indexOf(element);
+            collection.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+    // this method will return the size of the collection
+    this.size = function() {
+        return collection.length;
+    }
+    // this method will return the union of two sets
+    this.union = function(otherSet) {
+        var unionSet = new Set();
+        var firstSet = this.values();
+        var secondSet = otherSet.values();
+        firstSet.array.forEach(element => {
+            
+        });
+    }
+
+}
