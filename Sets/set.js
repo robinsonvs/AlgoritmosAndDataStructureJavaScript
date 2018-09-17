914214e9fd79dfc6d0129331a6e48c38af6899d5
Robinson Severo <robinsonvs@gmail.com>
2018-09-17 00:42:51 -0300

    commit 17092018 0042

--- Sets/set.js
+++ Sets/set.js
@@ -85,3 +85,19 @@ setB.add("c");
 setB.add("a");
 setB.add("d");
 console.log(setA.subset(setB));
+console.log(setA.intersection(setB).values());
+console.log(setB.difference(setA).values());
+
+var setC = new Set();
+var setD = new Set();
+setC.add("a");
+setD.add("b");
+setD.add("c");
+setD.add("a");
+setD.add("d");
+console.log(setD.values());
+setD.delete("a");
+console.log(setD.has("a"));
+console.log(setD.add("d"));
+
+
